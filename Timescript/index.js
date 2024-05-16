392993bc66f67153e2b6dc9f94c412a3f76717e8
mrazaalvi16 <167030559+mrazaalvi16@users.noreply.github.com>
2024-05-17 00:52:51 +0500

    Add files via upload

---
+++ Timescript/index.js
@@ -0,0 +1,38 @@
+// loope
+// for (let i = 0; i < 10; i++) {
+//     console.log("hello world", i);
+//     }
+//array
+// let student_name = ["Ali","Umer","Adil"];
+// console.log (student_name);
+// student_name.push("Aneeq")
+// console.log(student_name);
+// student_name.pop()
+// student_name.unshift("Aneeq")
+// student_name.shift()
+// console.log(student_name);
+// for (let i = 0; i < 10; i++) {
+//     console.log("hello world", i);
+// }
+// for (let i =1; i<51; i++);
+//     {
+//     if (i %5==0)
+//         {
+//             document.write(i +"<br>");
+//         }
+//                 }
+// let num = 2;
+// console.log("Table of" ,num);
+// for (let i =1 ; i <=10; i++){
+//     let Table = num *i;
+//     console.log(num , "X" , i , " = " , Table);
+// } 
+// let num = 3;
+// console.log("Table of" , num);
+// for (let i =1 ; i <=10; i++){
+//     let Table = num * i;
+//     console.log(num , "X" , i , " = " , Table);
+// }
+// task 2
+var personName = " Eric,";
+console.log("Hello" + personName + +" " + "Would you like to learn some Python today?");
